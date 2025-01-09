5645233d9f55c1a4d8ea669e2fe0f7ccdb8d98d5
macaumang <macaumang@gmail.com>
2025-01-09 01:02:02 -0600

    347

---
+++ 20250101/347. Top K Frequent Elements.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        counter = {}
+
+        for n in nums:
+            counter[n] = 1 + counter.get(n,0)
+        freq = [[] for _ in range(len(nums)+1)]
+
+        for n, f in counter.items():
+            freq[f].append(n)
+        res = []
+
+        for i in range(len(freq) -1,-1,-1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+        
+         
+
+
+
+
+        
