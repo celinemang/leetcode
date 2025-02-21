dc6a8c037ab8df024b66ea4ed4e5071d592da700
macaumang <macaumang@gmail.com>
2025-02-20 22:31:02 -0600

    286

---
+++ 286.Walls and Gates.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def wallsAndGates(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: None Do not return anything, modify rooms in-place instead.
+        """
+        def dfs(rooms, r, c,d):
+            for x, y in[(-1,0), (1,0), (0,-1), (0,1)]:
+                if 0 <= r+x< len(rooms) and 0<= c+y < len(rooms[0]) and rooms[r+x][c+y] > rooms[r][c]:
+                    rooms[r+x][c+y] = d+1
+                    dfs(rooms, r+x, c+y, d+1)
+        
+        if not rooms:
+            return[]
+        for r in range(len(rooms)):
+            for c in range(len(rooms[0])):
+                if rooms[r][c] == 0:
+                    dfs(rooms,r,c,0)
+
+        
