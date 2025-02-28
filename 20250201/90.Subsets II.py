2bddfa0cf4ba4da3a0b9a4f88be16efcffb21145
macaumang <macaumang@gmail.com>
2025-02-28 17:13:55 -0600

    79

---
+++ 20250201/90.Subsets II.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        n = len(nums)
+        # Sort the generated subset. This will help to identify duplicates.
+        nums.sort()
+        maxNumberOfSubsets = 2**n
+        # To store the previously seen sets.
+        seen = set()
+        subsets = []
+        for subsetIndex in range(maxNumberOfSubsets):
+            # Append subset corresponding to that bitmask.
+            currentSubset = []
+            hashcode = ""
+            for j in range(n):
+                # Generate the bitmask
+                mask = 2**j
+                isSet = mask & subsetIndex
+                if isSet != 0:
+                    currentSubset.append(nums[j])
+                    # Generate the hashcode by creating a comma separated string of numbers in the currentSubset.
+                    hashcode += str(nums[j]) + ","
+            if hashcode not in seen:
+                subsets.append(currentSubset)
+                seen.add(hashcode)
+        return subsets
+        
